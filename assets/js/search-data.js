3a4e73e05eccfc37b91af242ec4fa1497d7cdf23
roy-yuchenhe <royarthurhe@gmail.com>
2026-03-16 16:30:22 +0000

    Deploying to gh-pages from @ roy-yuchenhe/roy-yuchenhe.github.io@7fe7dbdedbebff467275b401dacbd3b0900a74f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,11 +469,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
