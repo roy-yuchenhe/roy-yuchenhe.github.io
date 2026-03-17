349bb00e4943db1c0f4a8cd8ceb284b329f3f5c2
roy-yuchenhe <royarthurhe@gmail.com>
2026-03-17 07:03:52 +0000

    Deploying to gh-pages from @ roy-yuchenhe/roy-yuchenhe.github.io@a82a523878fd2a9dfb7c6ead3defdf95e62d8c61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,6 +433,11 @@ ninja.data = [{
           description: "Variational models for image decomposition",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-image-reconstruction",
+          title: 'Image Reconstruction',
+          description: "Applications of variational principles and deep learning to image reconstruction.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
